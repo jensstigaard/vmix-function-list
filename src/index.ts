10424da7f017e9a3092526cbb037aac6f1141993
Jens Grønhøj Stigaard <jens@stjernholmco.dk>
2020-05-03 23:03:34 +0200

    Fixed export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import FunctionList from './function-list'
 export default FunctionList
 
 // For CommonJS default export support
-module.exports = FunctionList
-module.exports.default = FunctionList
+// module.exports = FunctionList
+// module.exports.default = FunctionList
