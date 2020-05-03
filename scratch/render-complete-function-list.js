f0831e8c41a5b2cde0ca09595fd96c3ee314a3fd
Jens Grønhøj Stigaard <jens@stjernholmco.dk>
2020-05-03 23:03:23 +0200

    Cleaned up render script

--- scratch/render-complete-function-list.js
+++ scratch/render-complete-function-list.js
@@ -1,22 +1,26 @@
 const fs = require('fs')
 const path = require('path')
 
-const { FunctionList } = require('../dist/index').default
+const FunctionList = require('../dist/index').default
 
 const list = new FunctionList()
 
+const dir = path.resolve(__dirname, '../rendered')
 
-const destinationFull = path.resolve(__dirname, '../complete-function-list', 'list.json')
-const destinationMin = path.resolve(__dirname, '../complete-function-list', 'list.min.json')
+const destinationFull = path.resolve(dir, 'list.json')
+const destinationMin = path.resolve(dir, 'list.min.json')
 
 const funcs = list.all()
 
+console.log('The complete list of functions in the vMix API were saved to:')
 // Save nicely formatted JSON to file
 // https://gist.github.com/collingo/6700069
 fs.writeFileSync(destinationFull, JSON.stringify(funcs, null, 2))
-console.log('The complete list of functions in the vMix API were saved to', destinationFull)
+console.log('(pretty)\t', destinationFull)
 
 // Minimised version JSON to file
 fs.writeFileSync(destinationMin, JSON.stringify(funcs))
-console.log('The complete list of functions in the vMix API were saved to', destinationMin)
+console.log('(minified)\t', destinationMin)
+
+console.log()
 
